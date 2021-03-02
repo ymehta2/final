d4864dd27f93e1e1feef3615e587353131d22b5c
Yash Mehta <yash.mehta@kellogg.northwestern.edu>
2021-03-01 18:05:26 -0600

    Commit 2

--- index.js
+++ index.js
@@ -20,4 +20,23 @@ firebase.auth().onAuthStateChanged(async function(user) {
     // Starts FirebaseUI Auth
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
+
+  let stocks = new Stocks('CF0SLZCQGPR7G3PA')
+  let result = await stocks.timeSeries({
+    symbol: 'TSLA',
+    interval: '1min',
+    amount: 1
+   });
+  
+  // document.querySelector('.stockSearch').insertAdjacentElement('beforeend', `
+  //  <h1>Stock ticker ${result.symbol}</h1>
+  // `)
+  
+  document.querySelector('.stockSearch').insertAdjacentHTML('beforeend', `
+  <div class="w-1/5 p-4">
+    ${result[0].close}
+  </div>
+`)
+console.log(result)
+  
 })
