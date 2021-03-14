a1a3a8c0743ade3bc594d2124e7742344398b117
Yash Mehta <yash.mehta@kellogg.northwestern.edu>
2021-03-14 17:28:47 -0500

    cOMMIT 5

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+ 
   if (user) {
-    // Signed in
-    console.log('signed in')
-  } else {
-    // Signed out
-    console.log('signed out')
+    let currentUserName = firebase.auth().currentUser.displayName
 
-    // Initializes FirebaseUI Auth
-    let ui = new firebaseui.auth.AuthUI(firebase.auth())
-
-    // FirebaseUI configuration
-    let authUIConfig = {
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      signInSuccessUrl: 'index.html'
-    }
-
-    // Starts FirebaseUI Auth
-    ui.start('.sign-in-or-sign-out', authUIConfig)
-  }
+   // Signout button
+   document.querySelector(".sign-in-or-sign-out").innerHTML = `
+   <p class="font-bold text-right pr-5">Welcome ${currentUserName}</p>
+   <a href="#" class="sign-out-button text-pink-500 underline text-right pr-5">Sign Out</a>
+ `
+    document.querySelector(".sign-out-button").addEventListener("click", function(event){
+      event.preventDefault()
+      firebase.auth().signOut()
+      document.location.href = "index.html"
+    })
 
   document.querySelector('.submitButton').addEventListener('click', async function(event){
     event.preventDefault()
     document.querySelector('.stockSearch').innerHTML = ``
+    document.querySelector('.submitOrderDiv').innerHTML = ``
     
     let stockTicker = document.getElementById("stock-ticker-input").value
     let stockQuantity = document.getElementById("stock-quantity-input").value
@@ -40,20 +34,72 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     document.querySelector('.stockSearch').insertAdjacentHTML('beforeend', `
     <div class="w-full p-4">
-    The stock price of ${stockTicker} as of ${result[0].date} was
+    The stock price of ${stockTicker} as of ${result[0].date} was $${result[0].close}
     </div>
 
-    <div class="w-full p-4 text-purple-500 font-bold text-2xl text-center">
-    $${result[0].close}
-    </div>
-
-    <div class="w-full p-4 text-purple-500 font-bold text-2xl text-center">
-    For ${stockQuantity} shares, it would cost ${orderCost}.
+    <div class="w-full p-4 text-blue-500 font-bold text-xl text-center">
+    Order Summary: For ${stockQuantity} share(s), it would cost $${orderCost}.
     </div>
 
     `)
+    
+    document.querySelector(".submitOrderDiv").insertAdjacentHTML('beforeend', `
+    <div class="w-full">
+    <button class="submitOrderButton bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-xl w-full">Submit Order</button>
+    </div>
+    `)
+
+    let db = firebase.firestore()
+    document.querySelector(".submitOrderButton").addEventListener("click", async function(event){
+    let docRef = await db.collection('Portfolio').doc(`${orderCost}-${stockQuantity}-${stockTicker}-${firebase.auth().currentUser.uid}`).set({
+      stockTicker: stockTicker,
+      stockOwned: stockQuantity,
+      orderTotal: orderCost
+    })
+    document.location.href = "index.html"
+    console.log(docRef)
+    })
+
+
 
   console.log(result)
-})
+  })
+    let db = firebase.firestore()
+    let querySnapshot = await db.collection('Portfolio').get()
+              
+      let items = querySnapshot.docs
 
-})
+      for (let i=0; i<items.length; i++) {
+        let item = items[i].data() // each member of the docs Array is a reference, so use .data() to get it into an Object
+        item.stockTicker // => e.g. 'grapes'
+       
+        
+      document.querySelector(".stockTable2").insertAdjacentHTML('beforeend', `
+      <tr>
+        <td class='border border-blue-300 text-center'>${item.stockTicker}</td>
+        <td class='border border-blue-300 text-center'>${item.stockOwned}</td>
+        <td class='border border-blue-300 text-center'>$${item.orderTotal}</td>
+      </tr>
+      `)
+      }
+
+      
+
+} else {
+
+  document.querySelector(".stockTable").classList.add("hidden")
+  // Initializes FirebaseUI Auth
+  let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+  // FirebaseUI configuration
+  let authUIConfig = {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: 'index.html'
+  }
+
+  // Starts FirebaseUI Auth
+  ui.start('.sign-in-or-sign-out', authUIConfig)
+
+}})
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyAvxF37Ihx9TmXbES_H-22jsCeeQoA5ga8",
+  authDomain: "kiei-451-cb972.firebaseapp.com",
+  projectId: "kiei-451-cb972",
+  storageBucket: "kiei-451-cb972.appspot.com",
+  messagingSenderId: "264975974212",
+  appId: "1:264975974212:web:4f2476f2995fa68b2a2111"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
